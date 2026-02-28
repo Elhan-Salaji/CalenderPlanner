d9fe1b266b17f5e49a8955f147fee4619d196072
Salaji Elhan <es171@hdm-stuttgart.de>
2026-02-28 04:12:12 +0100

    Initial commit: Spring Boot setup with Web, Security, and Docker

---
+++ CalendarPlanner/src/main/java/de/elhan/calendarplanner/CalendarPlannerApplication.java
@@ -0,0 +1,13 @@
+package de.elhan.calendarplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarPlannerApplication.class, args);
+	}
+
+}
---
+++ CalendarPlanner/src/test/java/de/elhan/calendarplanner/CalendarPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.elhan.calendarplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
